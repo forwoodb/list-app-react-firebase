9bbbb34392898efdc566a1103cfdc57ed9039024
Brent Forwood <forwoodb77@gmail.com>
2020-10-01 13:31:50 -0400

    Adjusted create list layout

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -158,77 +158,77 @@ export default class Main extends Component {
           />
           <Route exact path="/">
             <h1 className="text-center">Create List</h1>
-            <div className="row">
-              <div className="col"></div>
-              <div className="col-6">
-                <form onSubmit={this.handleSubmit} style={this.getStyle()}>
-                  <div className="form-group">
-                    <input className="form-control" name="text"/>
-                  </div>
-                  <div className="form-group">
-                    <button className="btn btn-success">Add</button>
-                  </div>
-                </form>
-                <table className="table">
-                <thead>
-                  <tr>
-                    <th className="text-center">Lists</th>
-                    <th></th>
-                    <th></th>
-                  </tr>
-                </thead>
-                  <tbody>
-                    {
-                      this.state.lists.map((list) => {
-                        if (list.edit) {
-                          return (
-                            <tr key={list.id}>
-                              <td className="text-center">
-                                <input className="text-center" name="update" value={list.list} onChange={this.handleUpdateChange}/>
-                              </td>
-                              <td className="text-center">
-                                <button className="btn btn-danger" onClick={() => this.deleteList(list.id)}>Delete</button>
-                              </td>
-                              <td className="text-center">
-                                <button className="btn btn-success" onClick={() => this.updateList(list.id)}>Done</button>
-                              </td>
-                            </tr>
-                          )
-                        } else {
-                          return (
-                            <tr key={list.id}>
-                              <td className="text-center"  width="240px">{list.list}</td>
-                              <td className="text-center">
-                                <button
-                                  className="btn btn-danger"
-                                  onClick={() => this.deleteList(list.id)}
-                                >
-                                  Delete
-                                </button>
-                              </td>
-                              <td className="text-center">
-                                <button className="btn btn-primary" onClick={() => this.editItem(list.id)}>Edit</button>
-                              </td>
-                              <td className="text-center">
-                                <Link to={'/View/' + list.id}>
-                                  <button
-                                    className="btn btn-success"
-                                  >
-                                    View
-                                  </button>
-                                </Link>
-                              </td>
-                            </tr>
-                          )
-                        }
-
-                      })
-                    }
-                  </tbody>
-                </table>
+            <form onSubmit={this.handleSubmit} style={this.getStyle()}>
+              <div className="form-group">
+                <input className="form-control" name="text"/>
               </div>
-              <div className="col"></div>
-            </div>
+              <div className="form-group">
+                <button className="btn btn-success">Add</button>
+              </div>
+            </form>
+            <table className="table table-sm">
+            <thead>
+              <tr>
+                <th className="text-center">Lists</th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+              <tbody>
+                {
+                  this.state.lists.map((list) => {
+                    if (list.edit) {
+                      return (
+                        <tr key={list.id}>
+                          <td className="text-center">
+                            <input className="text-center" name="update" value={list.list} onChange={this.handleUpdateChange}/>
+                          </td>
+                          <td className="text-center">
+                            <button className="btn btn-danger btn-sm" onClick={() => this.deleteList(list.id)}>Delete</button>
+                          </td>
+                          <td className="text-center">
+                            <button className="btn btn-success btn-sm" onClick={() => this.updateList(list.id)}>Done</button>
+                          </td>
+                        </tr>
+                      )
+                    } else {
+                      return (
+                        <tr key={list.id}>
+                          <td className="text-center">{list.list}</td>
+                          <td className="text-center">
+                            <button
+                              className="btn btn-danger btn-sm"
+                              onClick={() => this.deleteList(list.id)}
+                            >
+                              Delete
+                            </button>
+                          </td>
+                          <td className="text-center">
+                            <button
+                              className="btn btn-primary btn-sm"
+                              onClick={() => this.editItem(list.id)}
+                            >
+                              Edit
+                            </button>
+                          </td>
+                          <td className="text-center">
+                            <Link to={'/View/' + list.id}>
+                              <button
+                                className="btn btn-success btn-sm"
+                              >
+                                View
+                              </button>
+                            </Link>
+                          </td>
+                        </tr>
+                      )
+                    }
+
+                  })
+                }
+              </tbody>
+            </table>
           </Route>
           <Route path={'/View/:id'} exact component={ListItems}/>
         </Router>
